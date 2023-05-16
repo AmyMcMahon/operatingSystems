dfef2df5845fa9d00c34f2737990527a24b95575
AmyMcMahon <admcmahon@live.com[3~[3~>
2023-05-17 00:17:25 +0100

    Add message queue to server

--- implement/client.java
+++ implement/client.java
@@ -3,6 +3,8 @@ package implement;
 import java.net.*;
 import java.util.Scanner;
 import java.io.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class client {
     public static void main(String[] args) {
@@ -10,29 +12,38 @@ public class client {
         System.out.println("Welcome to the chatBot!, enter 'help' to see the functions of the chatbot");
         System.out.print("> ");
         boolean exit = false;
+
+        BlockingQueue<String> messageQueue = new LinkedBlockingQueue<>();
+        server serverInstance = new server();
+        Thread messageProcessorThread = new Thread(() -> serverInstance.processMessages(messageQueue));
+        messageProcessorThread.start();
+
         while (!exit) {
             String input = scannerIn.nextLine();
             if (input.equals("tell me a joke")) {
                 try {
                     Socket sock = new Socket("127.0.0.1", 6013);
-                    InputStream in = sock.getInputStream();
-                    BufferedReader bin = new BufferedReader(new InputStreamReader(in));
                     PrintWriter pout = new PrintWriter(sock.getOutputStream(), true);
+
+                    messageQueue.put(input);
 
                     while (true) {
                         // Send the user's input to the implement.server
                         pout.println(input);
 
                         // Wait for a response from the implement.server
-                        String serverResponse = bin.readLine();
+                        String serverResponse = messageQueue.take();
                         System.out.println(serverResponse);
                         System.out.print("> ");
                         input = scannerIn.nextLine();
                         if (input.equals("exit")) {
                             exit = true;
                         }
+
+                        messageQueue.put(input);
+
                     }
-                } catch (IOException ioe) {
+                } catch (IOException | InterruptedException ioe) {
                     System.err.println(ioe);
                 }
             }
