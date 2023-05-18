fc5b5bdfad084fb657b6f9ffb7efc44d3e73fb1c
AmyMcMahon <admcmahon@live.com[3~[3~>
2023-05-18 23:42:35 +0100

    messages

--- src/implement/Server.java
+++ src/implement/Server.java
@@ -8,20 +8,9 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class Server {
     private ServerSocket serverSocket;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         try {
             ServerSocket sock = new ServerSocket(6013);
-            BlockingQueue<String> messageQueue = new LinkedBlockingQueue<>();
-            Server serverInstance = new Server();
-            Thread messageProcessorT = new Thread(() -> {
-                try {
-                    serverInstance.processMessages(messageQueue);
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            });
-            messageProcessorT.start();
             while (true) {
                 Socket client = sock.accept();
                 BufferedReader bin = new BufferedReader(new InputStreamReader(client.getInputStream()));
@@ -29,7 +18,12 @@ public class Server {
                 String input;
                 while ((input = bin.readLine()) != null) {
                     if (input.equalsIgnoreCase("threads")) {
-                        messageQueue.put(input);
+                        Thread javaT = new Thread(new JokeTeller(JokeType.JavaJoke, pout));
+                        Thread batmanT = new Thread(new JokeTeller(JokeType.BatmanJoke, pout));
+                        Thread mathsT = new Thread(new JokeTeller(JokeType.MathsJoke, pout));
+                        javaT.start();
+                        batmanT.start();
+                        mathsT.start();
                     }
                 }
                 client.close();
@@ -50,29 +44,6 @@ public class Server {
                 Thread t = new Thread(clientThread);
                 t.start();
             } catch (IOException ignored) {
-            }
-        }
-    }
-
-    public void processMessages(BlockingQueue<String> messageQueue) throws IOException {
-        ServerSocket sock = new ServerSocket(6013);
-        Socket client = sock.accept();
-        PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-        while (true) {
-            try {
-                String message = messageQueue.take();
-                if (message.equals("threads")) {
-                    Thread javaT = new Thread(new JokeTeller(JokeType.JavaJoke, pout));
-                    Thread batmanT = new Thread(new JokeTeller(JokeType.BatmanJoke, pout));
-                    Thread mathsT = new Thread(new JokeTeller(JokeType.MathsJoke, pout));
-                    javaT.start();
-                    batmanT.start();
-                    mathsT.start();
-                }
-                client.close();
-
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
             }
         }
     }
